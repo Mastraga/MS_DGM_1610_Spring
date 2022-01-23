c11b5706d10aa4f197f01d7187ee1ef77be6e207
Mason Straga <10767492@uvu.edu>
2022-01-23 15:09:56 -0700

    Made new project, changed playmode tint

---
+++ First Project/Assets/Scenes/First Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("I am the Lorax");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

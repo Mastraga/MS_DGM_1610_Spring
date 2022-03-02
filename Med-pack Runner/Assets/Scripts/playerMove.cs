36dd16f7861fd9b3efe9bf454282967ce5c5165f
Mason Straga <10767492@uvu.edu>
2022-03-02 09:45:08 -0700

    Med-pack Runner is final project, mess around is what is sounds like

---
+++ Med-pack Runner/Assets/Scripts/playerMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    // 
+    public float hInput;
+    public float vInput;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+    }
+}
